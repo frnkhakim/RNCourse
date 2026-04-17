2f111bcb229d0b7cf43a6f4568fba486bc630b2b
FrankHakim <frank.hakim@digitatanetworks.com>
2026-04-17 08:22:03 +0200

    Latest changes

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Test A12\pp!</Text>  
+        <Text>Test A12\pp!</Text>  
+      </View>
+       <Text>Test A12\pp!</Text>  
+       <Text>Test A12\pp!</Text>  
+
+       <Button
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
 }
